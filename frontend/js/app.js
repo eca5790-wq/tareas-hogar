3dfd65d2c81e2ca57fac745d7f6f324f2a23b95d
eca5790-wq <e.c.a.5790@gmail.com>
2026-07-24 09:47:30 +0200

    Sprint 1 - Estructura inicial del frontend

---
+++ frontend/js/app.js
@@ -0,0 +1,7 @@
+import { loadHome } from "../pages/home.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    loadHome();
+
+});
---
+++ frontend/pages/ home.js
@@ -0,0 +1,13 @@
+export function loadHome() {
+
+    document.getElementById("header").innerHTML = `
+        <h2>Tareas Hogar</h2>
+    `;
+
+    document.getElementById("content").innerHTML = `
+        <h3>Inicio</h3>
+
+        <p>La aplicación se está iniciando...</p>
+    `;
+
+}
